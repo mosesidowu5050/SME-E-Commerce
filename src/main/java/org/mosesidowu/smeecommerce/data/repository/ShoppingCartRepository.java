73dc6383f7a0f1b4511dea2466b95fe16593e107
Ericalli <Ericalli>
2025-06-23 19:21:05 +0100

    started user login service

--- src/main/java/org/mosesidowu/smeecommerce/data/repository/ShoppingCartRepository.java
+++ src/main/java/org/mosesidowu/smeecommerce/data/repository/ShoppingCartRepository.java
@@ -1,12 +1,4 @@
-package org.mosesidowu.smeecommerce.services;
+package org.mosesidowu.smeecommerce.data.repository;
 
-import org.mosesidowu.smeecommerce.data.models.ShoppingCart;
-
-import javax.swing.text.html.Option;
-import java.util.Optional;
-
-public interface ShoppingCartService {
-
-    Optional<ShoppingCart> findByUserId(String userId);
-    
+public interface ShoppingCartRepository {
 }
--- src/main/java/org/mosesidowu/smeecommerce/services/UserServiceImpl.java
+++ src/main/java/org/mosesidowu/smeecommerce/services/UserServiceImpl.java
@@ -1,4 +1,9 @@
 package org.mosesidowu.smeecommerce.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl {
+
+
 }
