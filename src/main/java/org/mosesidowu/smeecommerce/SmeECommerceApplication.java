6656f8f526cfb1d47794ac79d92684fa3a9144c6
idowu moses <dynamopraize5050@gmail.com>
2025-06-23 11:43:19 +0100

    commit sme e-commerce project

---
+++ src/main/java/org/mosesidowu/smeecommerce/SmeECommerceApplication.java
@@ -0,0 +1,12 @@
+package org.mosesidowu.smeecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmeECommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmeECommerceApplication.class, args);
+    }
+}
---
+++ src/test/java/org/mosesidowu/smeecommerce/SmeECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mosesidowu.smeecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmeECommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
