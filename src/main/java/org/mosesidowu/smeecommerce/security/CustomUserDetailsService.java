17e804cf071b23f3505893a259b2a3b21310cad0
idowu moses <dynamopraize5050@gmail.com>
2025-06-23 18:26:57 +0100

    commit sme e-commerce project

---
+++ src/main/java/org/mosesidowu/smeecommerce/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.mosesidowu.smeecommerce.security;
+
+import org.mosesidowu.smeecommerce.data.models.User;
+import org.mosesidowu.smeecommerce.data.repository.UserRepository;
+import org.mosesidowu.smeecommerce.exception.UserException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findUsersByEmail(email).orElseThrow(() -> new UserException("User not found"));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                new ArrayList<>()
+        );
+    }
+}
