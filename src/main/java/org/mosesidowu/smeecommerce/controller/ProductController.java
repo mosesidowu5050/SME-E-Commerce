21af4daec1c5e0734454f902f86e9de19c423be9
Ericalli <Ericalli>
2025-06-27 17:48:15 +0100

    added validations for inputs

--- src/main/java/org/mosesidowu/smeecommerce/controller/ProductController.java
+++ src/main/java/org/mosesidowu/smeecommerce/controller/ProductController.java
@@ -1,13 +1,9 @@
 package org.mosesidowu.smeecommerce.controller;
 
 import org.mosesidowu.smeecommerce.dtos.requests.CreateProductRequest;
-import org.mosesidowu.smeecommerce.dtos.responses.ApiResponse;
 import org.mosesidowu.smeecommerce.dtos.responses.CreateProductResponse;
-import org.mosesidowu.smeecommerce.exception.UserException;
 import org.mosesidowu.smeecommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
--- src/main/java/org/mosesidowu/smeecommerce/dtos/requests/CreateSubAdminRequest.java
+++ src/main/java/org/mosesidowu/smeecommerce/dtos/requests/CreateSubAdminRequest.java
@@ -1,11 +1,16 @@
 package org.mosesidowu.smeecommerce.dtos.requests;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateSubAdminRequest {
-
+    @NotBlank(message = "name is required")
     private String fullName;
+
+    @NotBlank(message = "email is required")
+    @Email(message = "Enter a valid email")
     private String email;
 
 }
